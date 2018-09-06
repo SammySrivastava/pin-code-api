2cc87980b319b54fdbd3a7c98dae03d94d1ef5a2
Samarth Srivastava <samarth_srivastava@ymail.com>
2018-09-06 10:30:03 +0530

    Added dynamo db client and table

---
+++ pincodeapi/pincodeapi/Models/CreateDynamoDBClient.cs
@@ -0,0 +1,79 @@
+﻿using Amazon.DynamoDBv2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Web;
+
+namespace pincodeapi.Models
+{
+    public class CreateDynamoDBClient
+    {
+        // Global variables
+        public static bool operationSucceeded;
+        public static bool operationFailed;
+        public static AmazonDynamoDBClient client;
+        //public static Table moviesTable;
+        //public static TableDescription moviesTableDescription;
+        //public static CancellationTokenSource source = new CancellationTokenSource();
+        //public static CancellationToken token = source.Token;
+        //public static Document movie_record;
+
+        public static bool createClient(bool useDynamoDBLocal)
+        {
+            if (useDynamoDBLocal)
+            {
+                operationSucceeded = false;
+                operationFailed = false;
+
+                // First, check to see whether anyone is listening on the DynamoDB local port
+                // (by default, this is port 8000, so if you are using a different port, modify this accordingly)
+                bool localFound = false;
+                try
+                {
+                    using (var tcp_client = new TcpClient())
+                    {
+                        var result = tcp_client.BeginConnect("localhost", 8000, null, null);
+                        localFound = result.AsyncWaitHandle.WaitOne(3000); // Wait 3 seconds
+                        tcp_client.EndConnect(result);
+                    }
+                }
+                catch
+                {
+                    localFound = false;
+                }
+                if (!localFound)
+                {
+                    Console.WriteLine("\n      ERROR: DynamoDB Local does not appear to have been started..." +
+                                      "\n        (checked port 8000)");
+                    operationFailed = true;
+                    return (false);
+                }
+
+                // If DynamoDB-Local does seem to be running, so create a client
+                Console.WriteLine("  -- Setting up a DynamoDB-Local client (DynamoDB Local seems to be running)");
+                AmazonDynamoDBConfig ddbConfig = new AmazonDynamoDBConfig();
+                ddbConfig.ServiceURL = "http://localhost:8000";
+                try { client = new AmazonDynamoDBClient(ddbConfig); }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("     FAILED to create a DynamoDBLocal client; " + ex.Message);
+                    operationFailed = true;
+                    return false;
+                }
+            }
+
+            else
+            {
+                try { client = new AmazonDynamoDBClient(); }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("     FAILED to create a DynamoDB client; " + ex.Message);
+                    operationFailed = true;
+                }
+            }
+            operationSucceeded = true;
+            return true;
+        }
+    }
+}
